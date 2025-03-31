3aed72e67b260732f05e08612a5ef2d7872372fc
SHUBHAM2775 <shubhamu1332@gmail.com>
2025-03-31 11:26:34 +0530

    commit-2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import session from "express-session";
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -8,7 +9,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-let isLogin = false;
+// Add session middleware
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 } // 24 hours
+}));
 
 function contactUs(){
   console.log("contact us")
@@ -19,19 +26,18 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  if(isLogin === true){
-    res.sendFile(__dirname + "/public/welcome.html");
-  }
-  else{
-    res.sendFile(__dirname + "/public/index.html");
-  }
+    if (req.session.isLogin) {
+        res.redirect(`/welcome?name=${encodeURIComponent(req.session.name)}`);
+    } else {
+        res.sendFile(__dirname + "/public/index.html");
+    }
 });
 
 app.post("/action", (req, res) => {
-  const { name } = req.body; // Get the 'name' from the form submission
-  isLogin = true;
-  // Redirect to welcome.html and pass the name as a query parameter
-  res.redirect(`/welcome?name=${encodeURIComponent(name)}`);
+    const { name } = req.body;
+    req.session.isLogin = true;
+    req.session.name = name;
+    res.redirect(`/welcome?name=${encodeURIComponent(name)}`);
 });
 
 // Define the '/welcome' route that sends the welcome.html file
@@ -40,7 +46,11 @@ app.get("/welcome", (req, res) => {
 });
 
 app.get("/home", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+    if (req.session.isLogin) {
+        res.redirect(`/welcome?name=${encodeURIComponent(req.session.name)}`);
+    } else {
+        res.sendFile(__dirname + "/public/index.html");
+    }
 });
 
 app.get("/contact", (req, res) => {
@@ -66,6 +76,23 @@ app.get("/diet", (req, res) => {
 
 app.get("/book_class", (req, res) => {
   res.sendFile(__dirname + "/public/book_class.html");
+});
+
+app.get("/track", (req, res) => {
+    if (req.session.isLogin) {
+        res.sendFile(__dirname + "/public/track.html");
+    } else {
+        res.redirect('/');
+    }
+});
+
+app.get("/logout", (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect('/');
+    });
 });
 
 app.listen(port, () => {
